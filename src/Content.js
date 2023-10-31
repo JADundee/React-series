97116d756210f24e3d6ec698931fd3a528c05f20
JADundee <Jadundee@icloud.com>
2023-10-31 13:32:45 -0600

    Click events basics

--- src/Content.js
+++ src/Content.js
@@ -4,11 +4,28 @@ const Content = () => {
         const int = Math.floor(Math.random() * 4);
         return names[int];
       }
+
+      const handleClick = () => {
+        console.log('Clicked')
+      }
+
+      const handleClick2 = (name) => {
+        console.log(`${name} was clicked`)
+      }
+
+      const handleClick3 = (e) => {
+        console.log(e.target.innerText)
+      }
+      
+
     return (
         <main>
-            <p>
+            <p onDoubleClick={handleClick}>
                 Hello {handleNameChange()}!
             </p>
+            <button onClick={handleClick}>Click</button>
+            <button onClick={() => handleClick2('Bobert')}>Click</button>
+            <button onClick={(e) => handleClick3(e)}>Click</button>
         </main>
     )
 }
