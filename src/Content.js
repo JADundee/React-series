97401c4845ecbc50d2ed6329c6f0424cc5af4fc2
JADundee <Jadundee@icloud.com>
2023-11-01 06:11:12 -0600

    useState hooks

--- src/Content.js
+++ src/Content.js
@@ -1,31 +1,43 @@
+import { useState } from "react";
+
+
+
 const Content = () => {
-    const handleNameChange = () => {
+  const [name, setName] = useState('Randy')
+  const [count, setCount] = useState(0)
+    
+  const handleNameChange = () => {
+    const names = ['Bobert', 'Randy', 'Bobandy', 'Jimmy'];
+    const int = Math.floor(Math.random() * 4);
+    setName(names[int]);
+  }
+
+  // Vanilla JS way
+    /* const handleNameChange = () => {
         const names = ['Bobert', 'Randy', 'Bobandy', 'Jimmy'];
         const int = Math.floor(Math.random() * 4);
         return names[int];
-      }
+      } */
 
       const handleClick = () => {
-        console.log('Clicked')
+        setCount(count + 1)
+        setCount(count + 1)
+        console.log(count)
       }
 
-      const handleClick2 = (name) => {
-        console.log(`${name} was clicked`)
-      }
-
-      const handleClick3 = (e) => {
-        console.log(e.target.innerText)
+      const handleClick2 = () => {
+        console.log(count)
       }
       
 
     return (
         <main>
             <p onDoubleClick={handleClick}>
-                Hello {handleNameChange()}!
+                Hello {name}!
             </p>
+            <button onClick={handleNameChange}>Clicky</button>
             <button onClick={handleClick}>Click</button>
-            <button onClick={() => handleClick2('Bobert')}>Click</button>
-            <button onClick={(e) => handleClick3(e)}>Click</button>
+            <button onClick={handleClick2}>Click</button>
         </main>
     )
 }
